6b92d9c9d4c61c6fde50a587747321fb981fa078
zhaowenjun <zhaowjzhzj@163.com>
2019-03-21 16:50:51 +0800

    测试提交

---
+++ day01/base_type.py
@@ -0,0 +1,25 @@
+
+
+# 声明一个int_demo 方法
+def int_demo():
+    # 声明anit变量, 并赋予值 1
+    aint = 1
+    # 打印aint的值
+    print(aint)
+    print(type(aint))
+
+
+# 声明一个add方法 用两个参数: aint,bint
+def add(aint, bint):
+    print(aint)
+    print(bint)
+    return aint - bint
+
+def float_demo():
+    # 声明float变量, 并赋予值 1
+    afloat = 1.88
+    # 打印afloar的值
+    print(afloat)
+    print(type(afloat))
+
+if __name__ == '__main__':
